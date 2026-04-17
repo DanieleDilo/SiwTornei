4660c0910295589d46ff25672719817176afdcc1
DanieleDilo <dan.dilorenzo2@stud.uniroma3.it>
2026-04-17 16:00:21 +0200

    Inizio progetto SIW - Struttura base

---
+++ src/main/java/it/uniroma3/siw/siw_tornei/SiwTorneiApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siw_tornei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwTorneiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwTorneiApplication.class, args);
+	}
+
+}
