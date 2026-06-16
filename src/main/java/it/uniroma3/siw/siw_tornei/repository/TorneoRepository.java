2d18cdaf63d69fa8fbe130597b332ea6130b97d9
DanieleDilo <dan.dilorenzo2@stud.uniroma3.it>
2026-06-16 17:04:30 +0200

    lazy fetch in torneo

--- src/main/java/it/uniroma3/siw/siw_tornei/repository/TorneoRepository.java
+++ src/main/java/it/uniroma3/siw/siw_tornei/repository/TorneoRepository.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 import java.util.List;
 
 public interface TorneoRepository extends CrudRepository<Torneo, Long> {
+
     
     @Query("SELECT DISTINCT t FROM Torneo t LEFT JOIN FETCH t.squadre WHERE t.id = :id")
     Optional<Torneo> findByIdWithSquadre(@Param("id") Long id);
@@ -21,5 +22,7 @@ public interface TorneoRepository extends CrudRepository<Torneo, Long> {
     @Query("SELECT t FROM Torneo t WHERE t.id = :id")
     Optional<Torneo> findByIdWithSquadreEntityGraph(@Param("id") Long id);
 
+    @Query("SELECT t FROM Torneo t)") //lazy
+    List<Torneo> findAll();
 
     }
